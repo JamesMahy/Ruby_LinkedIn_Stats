24ba859c4367c87f4ce8e479d60a62bb6b6eb6aa
James Mahy <james.mahy@civica.co.uk>
2018-10-08 11:22:24 +0100

    Updates for RubyCop Style guide

--- main.rb
+++ main.rb
@@ -1,22 +1,25 @@
-require "mini_magick"
-require "json"
-require "open-uri"
-require "fileutils"
+require 'mini_magick'
+require 'json'
+require 'open-uri'
+require 'fileutils'
 
 module MiniMagick
+  # Extends the image class to include the greyscale? method
   class Image
     # Adapted from https://www.imagemagick.org/discourse-server/viewtopic.php?t=19580&start=15
-    def is_grayscale
-      result = run_command("convert", "#{path}","-colorspace", "HSI", "-channel", "g", "-separate", "+channel", "-format", "%[fx:mean]", "info:").to_f
-      if result > 0
-        return false
-      else
-        return true
-      end
+    def greyscale?
+      result = run_command('convert', path, '-colorspace', 'HSI',
+                           '-channel', 'g', '-separate', '+channel',
+                           '-format', '%[fx:mean]', 'info:').to_f
+
+      return true unless result > 0
+
+      false
     end
   end
 end
 
+# Person class
 class Person
   attr_reader :name, :occupation, :public_identifier, :image_root, :image
 
@@ -28,92 +31,77 @@ class Person
     @image = image
 
     download_image
-
   end
 
-  def get_image_uri
+  def image_uri
     "#{@image_root}#{@image}"
   end
 
   def get_local_image_path(exists)
     path = "#{Dir.pwd}/data/images/#{@public_identifier}.jpg"
-    if !exists || File.file?(path)
-      return path
-    end
-    return nil
+    !exists || File.file?(path) ? path : nil
   end
 
   def download_image
     future_file = get_local_image_path(false)
     if @image_root && @image
       begin
-        if !File.file?(future_file)
-          puts "Downloading: #{get_image}"
-          tempfile = open(get_image)
+        unless File.file?(future_file)
+          puts "Downloading: #{image_uri}"
+          tempfile = File.open(image_uri)
           IO.copy_stream(tempfile, future_file)
         end
       rescue OpenURI::HTTPError
-          FileUtils.cp('wizard_color.jpg', future_file)
+        FileUtils.cp('wizard_color.jpg', future_file)
       end
     else
       FileUtils.cp('wizard_color.jpg', future_file)
     end
   end
-
 end
 
-def process_person (person)
-  if person && person['firstName']
+def process_person(person)
+  if person && person['firstName'] # rubocop:disable Style/GuardClause
 
     person_name = "#{person['firstName']} #{person['lastName']}"
     occupation = person['occupation']
     public_identifier = person['publicIdentifier']
+    image_root = nil
+    image = nil
 
     if person['picture']
       image_root = person['picture']['rootUrl']
       image = person['picture']['artifacts'][0]['fileIdentifyingUrlPathSegment']
     end
 
-    person = Person.new(name: person_name, occupation: occupation, public_identifier: public_identifier, image_root: image_root, image: image)
+    person = Person.new(name: person_name,
+                        occupation: occupation,
+                        public_identifier: public_identifier,
+                        image_root: image_root,
+                        image: image)
 
     image_path = person.get_local_image_path(true)
-    if image_path != nil
+
+    unless image_path.nil? || image.nil?
       image = MiniMagick::Image.open(image_path)
-      puts "#{person.occupation} - #{image.is_grayscale}"
+      puts "#{person.occupation} - #{image.greyscale?}"
     end
 
-    return person
+    person
 
   end
 end
 
-def process_dataset (dataset)
-  elements_array = dataset["included"]
-  elements_array.each do |person|
-    person_object = process_person(person)
-    if person_object
-
-    end
-  end
+def process_data_set(data_set)
+  elements_array = data_set['included']
+  elements_array.each { |person| process_person(person) }
 end
 
-def process_jsonfile
-  file = File.read("data/data.json")
+def process_json_file
+  file = File.read('data/data.json')
   arr = JSON.parse(file)
 
-  arr.each {|dataset|
-    if dataset
-      process_dataset(dataset)
-    end
-  }
+  arr.each { |data_set| data_set ? process_data_set(data_set) : nil }
 end
 
-#image = MiniMagick::Image.open("wizard_greyscale.png")
-#puts image.is_grayscale
-#
-#
-
-process_jsonfile
-
-puts "hello"
-
+process_json_file
